5a6cb07f87f671a00f96d088a1cd89f7ab8403be
tjay <jaikumartjay@gmail.com>
2024-07-12 17:50:49 +0530

    sec testing cm

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+function Gt(){
+  return <div><h1>hii</h1></div>
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App/>
+    <Gt/>
   ,
 )
